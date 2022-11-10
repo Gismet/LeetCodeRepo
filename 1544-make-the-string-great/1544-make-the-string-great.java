b3cca671374d77bdd1f628f30e8e149543a9aba2
Gismet Majidov <93595721+Gismet@users.noreply.github.com>
2022-11-11 01:01:08 +0400

    Update 1544-make-the-string-great.java

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,3 +1,19 @@
+/*1544. Make the string great
+Given a string s of lower and upper case English letters.
+
+A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+
+0 <= i <= s.length - 2
+s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+
+Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+
+Notice that an empty string is also good.
+*/
+
+// One trick: Whenever you feel like stuck on a problem in which you are working with characters and need some kind of range, check out ascii valus of characters. 
+
 class Solution {
     public String makeGood(String s) {
          StringBuilder str = new StringBuilder();
